8871d1cac3ea6888b5114dde92d969cb8a1a5c08
Alejandro Gomez <alejandro.gomez@rappi.com>
2017-06-16 17:58:29 -0500

    needs to be finished recycler

--- RecyclerViewGlide/app/src/main/java/com/example/root/recyclerviewglide/CustomAdapter.java
+++ RecyclerViewGlide/app/src/main/java/com/example/root/recyclerviewglide/CustomAdapter.java
@@ -1,35 +1,45 @@
 package com.example.root.recyclerviewglide;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by root on 15/06/17.
- */
 
 public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private List<CustomItemModel> itemModels;
+    public static final int TYPE_HEADER = 1;
+    public static final int TYPE_ITEM = 2;
+
+    private List<CustomModel> itemModels;
 
     public CustomAdapter() {
         this.itemModels = new ArrayList<>();
     }
 
-    public void addItem(CustomItemModel item){
+    public void addItem(CustomModel item){
         itemModels.add(item);
     }
 
-    public void addArrayItems(List<CustomItemModel> items){
+    public void addArrayItems(List<CustomModel> items){
         itemModels.addAll(items);
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        switch (viewType){
+            case TYPE_HEADER:
+                View itemView = layoutInflater.inflate(R.layout.item_header, parent, false);
+                return new ViewHolderHeader(itemView);
+            default:
+//                return new ViewHolderItem()
+                return null;
+
+        }
     }
 
     @Override
@@ -45,7 +55,7 @@ public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     @Override
     public int getItemCount() {
         int totalSize = 0;
-        for (CustomItemModel item: itemModels) {
+        for (CustomModel item: itemModels) {
             totalSize += item.getItems().size();
         }
         return totalSize;
